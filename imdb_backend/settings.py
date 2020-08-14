6f0321bc239ea5dd2c87ea0d9369823fcab1bf9a
IP <ilamparithi.karthikeyan@ionixxtech.com>
2020-08-14 11:47:43 +0530

    initial commit

--- imdb_backend/settings.py
+++ imdb_backend/settings.py
@@ -128,9 +128,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dir'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static_dir'),
+# )
 AUTH_USER_MODEL = "users.User"
 SWAGGER_SETTINGS = {
 	'SECURITY_DEFINITIONS': {
